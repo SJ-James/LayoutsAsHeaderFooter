d850f8a7efd1f854d5e262b1c16eeee75d15ce65
SJ-James <stephen@gritty-social.com>
2018-03-05 18:35:57 +0000

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php if ( 'on' == et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
+
+	<span class="et_pb_scroll_top et-pb-icon"></span>
+
+<?php endif;
+
+if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
+
+			<footer id="main-footer">
+				<?php
+				    // Global Module Shortcode - Get the ID from the customizer.
+                    $footersection = get_option( 'sj_divi_new_footer' );
+
+					echo do_shortcode('[et_pb_section global_module="' . $footersection . '"]');
+				?>
+			</footer> <!-- #main-footer -->
+		</div> <!-- #et-main-area -->
+
+<?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
+
+	</div> <!-- #page-container -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
